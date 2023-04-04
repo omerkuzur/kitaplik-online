edbc745711c1ff9181b5e188f481d71584f337a2
omerkuzur <omerkuzur@gmail.com>
2023-04-04 17:17:59 +0300

    first

---
+++ library-service/src/main/java/com/kitaplik/libraryservice/dto/BookDto.kt
@@ -0,0 +1,7 @@
+package com.kitaplik.libraryservice.dto
+
+data class BookDto @JvmOverloads constructor (val id: BookIdDto? = null,
+                   val title: String? = "",
+                   val year: Int? = 0,
+                   val author: String? = "",
+                   val pressName: String? = "")
